28a73d97272a7870a9b90c06776aec30c5b1b5b9
islemamirhassen <islem.ahmed92@gmail.com>
2024-11-17 16:37:13 +0100

    Enregistrer

---
+++ config.js
@@ -0,0 +1,2 @@
+const listeMots=["Cachalot", "Pétunia","Serviette"];
+const listePhrases=["exercice en js", "axercice en css","exercice en html"];
--- exercice.js
+++ exercice.js
@@ -1,3 +1,33 @@
- function afficheResultat(resultat , NombreTotaleMots , resul  ){}
+function afficheResultat(score , NombreTotaleMots){
+console.log("le score est " + score + " sur " +NombreTotaleMots)
+}
+function choisirPhrasesOuMots(){
+    let choix=prompt("veuillez liste des mots ou des phrases")
+    while(choix!=="mots"&& choix!=="phrases"){
+        choix=prompt("veuillez liste des mots ou des phrases")
+}
+    return choix;
+}
+function lancerBoucleDejeux(listprosition){
+    let score=0
+    for (let i=0 ;i<=listprosition.length ; i++ ){
+            let motUtilisateur=prompt("le mot est:" +listprosition[i])
+            if(motUtilisateur===listprosition[i])
+            score++ 
+        }
+        return score
+}
+    function lancerJeu(){
+        let choix=choisirPhrasesOuMots()
+        let NombreTotaleMots=0
+        if(choix==="mots"){
+        score= lancerBoucleDejeux(listeMots)
+            NombreTotaleMots=listeMots.length
+        }else{
+        score= lancerBoucleDejeux(listePhrases)
+            NombreTotaleMots=listePhrases.length
+        }
+        afficheResultat(score,NombreTotaleMots)
 
-
+    }
+    lancerJeu()
